c36caa25e0d86b4e757c4f63134c2f03678535d8
Cassidy <75055778+cassidychu@users.noreply.github.com>
2022-11-29 11:17:25 -0800

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,9 +12,25 @@ function Banner(){
     <div className="w-full flex justify-between h-16 mb-2">
 
       <div className="border-2 border-blue-300 bg-blue-400 drop-shadow-lg flex justify-center h-full w-16 rounded-md">
-        <img className="w-9/12 drop-shadow-lg" src={logo} alt='logo'/>
+        <a href="https://github.com/" target="_blank" alt="github">
+          <img className="w-9/12 drop-shadow-lg" src={logo} alt='logo'/>
+        </a>
       </div>
-
+      <div className="border-2 border-blue-300 bg-blue-400 drop-shadow-lg flex justify-center h-full w-16 rounded-md">
+        <a href="https://www.linkedin.com/" target="_blank" alt="linkedin">
+          <img className="w-9/12 drop-shadow-lg" src={logo} alt='logo'/>
+        </a>
+      </div>
+      <div className="border-2 border-blue-300 bg-blue-400 drop-shadow-lg flex justify-center h-full w-16 rounded-md">
+        <a href="https://stackoverflow.com/" target="_blank" alt="stackoverflow">
+          <img className="w-9/12 drop-shadow-lg" src={logo} alt='logo'/>
+        </a>
+      </div>
+      <div className="border-2 border-blue-300 bg-blue-400 drop-shadow-lg flex justify-center h-full w-16 rounded-md">
+        <a href="https://leetcode.com/" target="_blank" alt="leetcode">
+          <img className="w-9/12 drop-shadow-lg" src={logo} alt='logo'/>
+        </a>
+      </div>
       <div className="w-1/2 border-2 border-blue-300 drop-shadow-lg bg-blue-400 rounded-md flex justify-center align-text-middle pl-3 pr-3 pt-1 pb-1">
         <p className="inline-block font-bold drop-shadow-lg pt-3 m-0"> {'"'+ someQuote +'" -' + author} </p>
       </div>
@@ -32,6 +48,23 @@ function Banner(){
 export default Banner
 
 
+
+
+
+
+/* : Zen quotes Api, if we ever want to include
+const api_url ="https://zenquotes.io/api/quotes/";
+
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+}
+
+getapi(api_url);
+
+*/
 
 
 
