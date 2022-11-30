194dfe7c5dbcc869a6130b9e1b77d47fb2ffaf7f
aceracer3462 <arceracer.3462@gmail.com>
2022-11-29 17:36:06 -0800

    updated color picker with palette (still in progress)

--- src/components/ColorWheel.jsx
+++ src/components/ColorWheel.jsx
@@ -3,24 +3,24 @@ import { SketchPicker} from 'react-color';
 import { useState } from 'react';
 
 function ColorWheel() {
-    const [color, setColor] = useState({
-        activeColor: "#267E92",
+    const state = {
+        activeColor: "FFFFFF",
         presetColors: ["#FF001E", "#FF9F00", "#FFE900", "#B7FF00", "#00FF59", "#00FFBF",
-        "#00E5FF", "#0077FF", "#3300FF", "#A600FF", "#FF00E1", "#FF0062",
-        "#000000", "#9B9B9B", "#FFFFFF"]
-    });
+                    "#00E5FF", "#0077FF", "#3300FF", "#A600FF", "#FF00E1", "#FF0062",
+                    "#000000", "#9B9B9B", "#FFFFFF"]
+    }
     
 
-    const handleChangeComplete = (hexColor) => {
-        setColor({activeColor: hexColor.hex.toUpperCase()})
+    const handleChangeComplete = (colorHex) => {
+        this.setState({activeColor: colorHex.hex.toUpperCase()})
     }
 
-    const handleClick = (hexValue, activeColor) => {
-        setColor({activeColor: hexValue})
-        if (!color.presetColors.includes(activeColor)) {
-            setColor(color => {
-                const colors = color.presetColors.slice(0) 
-                colors.unshift(activeColor)
+    const handleClick = (id, currentColor) => {
+        this.props.addColor(id, currentColor)
+        if (!state.presetColors.includes(currentColor)) {
+            this.setState(state => {
+                const colors = state.presetColors.slice(0) 
+                colors.unshift(currentColor)
                 colors.pop()
                 return {
                     presetColors: colors 
@@ -33,15 +33,12 @@ function ColorWheel() {
         <div className="App" style={{ display: "flex", justifyContent: "space-around" }}>
             <div className="sketchpicker">
                 <SketchPicker
-                    color={color}
-                    onChange={ color => {
-                        setColor(color.hex);
-                    }}
-                    // onChangeComplete={handleChangeComplete}
+                    className='text-black'
+                    color={state.activeColor}
+                    onChangeComplete={handleChangeComplete}
                     disableAlpha={true}
                     width="96%"
-                    // activeColor={color.activeColor}
-                    presetColors={color.presetColors}
+                    presetColors={state.presetColors}
                 />
                 <button onClick={handleClick}>Confirm</button>
             </div>
