dc647fbe290aed9f0b375e564d8e630f0e97ccc5
Cassidy <75055778+cassidychu@users.noreply.github.com>
2022-12-06 23:56:05 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import Banner  from './components/Banner.jsx'
-import Timer  from './components/Timer.jsx'
+import WeatherApp from './components/WeatherApp.jsx';
+import SnippetsApp from './components/SnippetsApp';
+import Calendar from './components/Calendar.jsx';
+import Banner  from './components/Banner.jsx';
+import NotesApp from './components/NotesApp';
+import Timer  from './components/Timer.jsx';
+import Links from './components/Links.jsx';
+import ToDoApp from './components/ToDoApp';
 
-import { PlayIcon } from '@radix-ui/react-icons'
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App flex justify-center  h-screen w-screen bg-gradient-to-br from-sky-500 to-blue-500">
+    <div className="App flex justify-center  h-screen w-screen from-[#9DAB86]">
       <div className="w-4/5 h-full pt-16 ">
-
         <Banner/>
-
-
         <div className="grid grid-cols-3 gap-2 h-4/5">
-          <div className='bg-blue-200'>
-            <PlayIcon/>
+          <div className='from-[#9DAB86] grid grid-row gap-1'>
+            <Links/>
+            <NotesApp/>
+
           </div>
-          <div className='bg-blue-300'> col-2 </div>
+          
+          <div className='flex flex-col justify-between'> 
+           <ToDoApp/>
+           <Calendar/>
+           <Timer/>
+            
+    
+           
+          </div>
+
           <div className=''>
-            <Timer/>
+            <WeatherApp/>
+            <SnippetsApp/>
           </div>
         </div>
 
@@ -32,3 +46,24 @@ export default App;
 
 
 // installed an icon package refer: https://icons.radix-ui.com
+/*
+<div className="App flex justify-center  h-screen w-screen bg-gradient-to-br from-sky-500 to-blue-500">
+      <div className="w-4/5 h-full pt-16 ">
+        <Banner/>
+        <div className="grid grid-cols-3 gap-2 h-4/5">
+          <div className='bg-blue-200 grid grid-row gap-1'>
+            <Links/>
+            <NotesApp/>
+          </div>
+          <div className='bg-blue-300'> 
+           <WeatherApp/>
+           <ToDoApp/>
+          </div>
+          <div className=''>
+            <Timer/>
+            <SnippetsApp/>
+          </div>
+        </div>
+      </div>
+    </div>
+*/
