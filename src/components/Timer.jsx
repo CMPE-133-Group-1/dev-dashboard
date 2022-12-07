51a8a30e4c40753962ec5d7b50827a17e0d095a5
Cassidy <75055778+cassidychu@users.noreply.github.com>
2022-12-06 23:55:05 -0800

    Update Timer.jsx

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,29 +1,127 @@
-import React from 'react'
-import { PlayIcon, StopIcon } from '@radix-ui/react-icons'
+import React, {useEffect, useState} from 'react'
+import { PlayIcon, ReloadIcon, StopIcon } from '@radix-ui/react-icons'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 
+//progress bar from https://www.npmjs.com/package/react-circular-progressbar
 function Timer(){
-  return(
-    <div className="bg-blue-400 border-2 border-blue-300 rounded-md pt-4 pb-4 flex flex-col gap-6">
+  const [min, setMin] = useState(0)
+  const [sec, setSec] = useState(0)
+  const [selectedTime, setSelectedTime] = useState(0);
+  //const [displayMsg, setDisplayMsg] = useState(false)
+  const [pause, setPause] = useState(1)
 
-      <h1 className="text-XXL"> {'0'+':'+'00'} </h1>
+    useEffect(()=> {
+        if(pause===0){
+        let interval = setInterval( () => {
+            clearInterval(interval)
+              if(sec === 0){
+                if(min !== 0){
+                  setSec(59)
+                  setMin(min-1)
+                }else{
+                  //let minutes = displayMsg ? min : 4;
+                  //let seconds = 59;
+                  alert("Time for a break!")
+                  //setSec(seconds)
+                  //setMin(minutes)
+                  //setDisplayMsg(!displayMsg)
+                }
+              } else {
+                setSec(sec-1)
+              }
+        },1000)
+      }
+    }, [sec,pause])
+
+    let getTotalTime = (min, sec, selected) => {
+      return (((min * 60) + sec) / (selected)) * 100;
+    }
+    
+    let updateSelectedTime = (newMin) => {
+      setSelectedTime((newMin * 60) + sec);
+    }
+  
+  const timerMin = min < 10 ? `0${min}` : min;
+  const timerSec = sec < 10 ? `0${sec}` : sec;
+
+  return(
+    <div className="bg-Timer border-2 border-from-[#74634F] rounded-md pt-4 pb-4 flex flex-col gap-6">
+      <div style={{ width: "30%", margin: "auto"}}>
+        <CircularProgressbar 
+          value={getTotalTime(min, sec, selectedTime)} 
+          text={timerMin+':'+timerSec}
+        />
+      </div>
+      
+
+
+      <h1 className="text-XXL">  </h1>
 
       <div className="flex flex-row justify-around">
-        <button className="shadow-lg bg-blue-500 hover:bg-blue-600 rounded-md p-1 pl-3 pr-3"> 15 min </button>
-        <button className="shadow-lg bg-blue-500 hover:bg-blue-600 rounded-md p-1 pl-3 pr-3"> 30 min </button>
-        <button className="shadow-lg bg-blue-500 hover:bg-blue-600 rounded-md p-1 pl-3 pr-3"> 60 min </button>
-        <button className="shadow-lg bg-blue-500 hover:bg-blue-600 rounded-md p-1 pl-3 pr-3"> custom </button>
+        <button className="shadow-lg bg-TimerButton hover:bg-blue-600 rounded-md p-1 pl-3 pr-3"
+        onClick={() => { 
+          setMin(15); 
+          updateSelectedTime(15);
+        }}> 15 min </button>
+
+        <button className="shadow-lg bg-TimerButton hover:bg-blue-600 rounded-md p-1 pl-3 pr-3"
+        onClick={() => { 
+          setMin(30); 
+          updateSelectedTime(30);
+        }}> 30 min </button>
+
+        <button className="shadow-lg bg-TimerButton hover:bg-blue-600 rounded-md p-1 pl-3 pr-3"
+        onClick={() => { 
+          setMin(60);
+          updateSelectedTime(60);
+        }}> 60 min </button>
+
+        <div className=' shadow-lg bg-TimerButton hover:bg-blue-600 rounded-md p-1 pl-3 pr-3'> 
+          <input 
+          type="number" 
+          id="min" name="min" 
+          min="1"  
+          placeholder='Mins'
+          className=' bg-TimerButton pl-2'
+          onChange={(event) => {
+            setMin(event.target.value);
+            updateSelectedTime(event.target.value);
+          }}
+          />
+        </div>
       </div>
 
       <div className="flex flex-row justify-around w-1/2 ml-auto mr-auto ">
-        <button className="shadow-lg bg-green-500 hover:bg-green-600 rounded-full p-1 pl-3 pr-3">
-          <PlayIcon/>
-         </button>
-        <button className="shadow-lg bg-red-500 hover:bg-red-600 rounded-full p-1 pl-3 pr-3">
-          <StopIcon/>
+        <button
+            className='shadow-lg bg-green-500 hover:bg-green-600 rounded-md p-1 pl-3 pr-3'
+            onClick={() => { 
+              setPause(0)
+              console.log("Start"+pause);
+              }}>
+            <PlayIcon/>  
+        </button>
+        <button
+            className='shadow-lg bg-red-500 hover:bg-red-600 rounded-md p-1 pl-3 pr-3'
+            onClick={() => { 
+              setPause(1)
+              console.log("Stop"+pause); 
+              }}>
+            <StopIcon/>  
+        </button>
+        <button
+            className='shadow-lg bg-TimerButton hover:bg-blue-600 rounded-md p-1 pl-3 pr-3'
+            onClick={() => { 
+              setPause(1)
+              setMin(0)
+              setSec(0)
+              console.log("reset"+pause); 
+              
+              }}>
+            <ReloadIcon/>  
         </button>
       </div>
-
     </div>
   )
 }
@@ -31,3 +129,12 @@ function Timer(){
 
 
 export default Timer;
+
+
+
+/*
+<button className="shadow-lg bg-green-500 hover:bg-green-600 rounded-full p-1 pl-3 pr-3" onClick={startTimer()}/>
+        <button className="shadow-lg bg-red-500 hover:bg-red-600 rounded-full p-1 pl-3 pr-3">
+          <StopIcon/>
+        </button>
+*/
