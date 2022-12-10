dbcf73d432f18e58c0e708926bbc4d76c236a8e8
Cassidy <75055778+cassidychu@users.noreply.github.com>
2022-12-09 17:56:02 -0800

    Update NotesApp.jsx

--- src/components/NotesApp.jsx
+++ src/components/NotesApp.jsx
@@ -5,35 +5,44 @@ import { collection, doc, getDocs, addDoc, deleteDoc } from 'firebase/firestore'
 
 
 function NotesApp() {
-  // body and tite of the note objects
   const [newBody, setNewBody] = useState("")
   const [newTitle, setNewTitle] = useState("")
-  //
   const [notes, setNotes] = useState([])
   const notesCollectionRef = collection(db, "notes")
 
-  // Create an entry, passing in the reference to the collection and also the parameters of the note objects 
+  // Create an entry, passing in the reference tot the collection and also the parameters 
   const createNote = async () => {
-    if(newTitle!="" && newBody!=""){
-      await addDoc(notesCollectionRef, {Title: newTitle, Body: newBody})
-    }
+    await addDoc(notesCollectionRef, {Title: newTitle, Body: newBody})
+
   } 
 
+  // when the page rendes this will be called
   useEffect(()=>{
-    // we need an async, api q returns a promise. this promise will provide the objects stored in the DB
+    // we need an async, api q returns a promise. we store the data we aim to update
     const getNotes = async () => {
       const data = await getDocs(notesCollectionRef)
-      // will set our notes to equal the objects in teh db we are going to display.
+      // still documenting here...
       setNotes(data.docs.map((doc) => ({...doc.data(), id: doc.id}) ));
     }; 
     // here we call the function to refresh the results 
     getNotes()
     // eslint-disable-next-line
-    // if a change is detected in notes the page will re-render
   },[notes])
+  // above [means that if a change is detected in notes it will re-render!!]
+
   
 
-  // Deleting a note, pass in the id, update the db on value we aim to delete, then delete it and wait for the confirmation
+  // 
+  const updateNote = async (id, title, body) => {
+    // const noteDoc = doc(db, "notes", id)
+    // const newTitle = {title: title }
+    // const newBody = {body: body }
+    // await updateDoc(noteDoc,newTitle)
+    // await updateDoc(noteDoc,newBody)
+    console.log(id +" : "+ title + " : " + body)
+  }
+
+  // Deleting a note, we need to know which note, pass in the id, store the value we aim to delete, then delete it
   const deleteNote = async (id) => {
     const noteDoc = doc(db, "notes", id)
     await deleteDoc(noteDoc)
@@ -45,19 +54,23 @@ function NotesApp() {
         <div className=' flex flex-col  gap-1 rounded-lg p-2 mb-2'> 
           <input className='bg-grey-200 text-zinc-800 p-1 rounded-lg' placeholder='Title...' type='text' minLength={3} onChange={(event) => {setNewTitle(event.target.value)}}/> 
           <textarea className='bg-grey-200 text-zinc-800 p-1 rounded-lg' placeholder='Body...' type='text' minLength={3} onChange={(event)  => {setNewBody(event.target.value)}}/> 
-          <button className='rounded-lg w-1/2 mr-auto ml-auto' style={{background: "#A7BF93", border: '1px solid #74634F'}} name='create note' onClick={createNote}> submit </button>
+          <button className=' rounded-lg w-1/2 mr-auto ml-auto' name='create note' onClick={createNote} style={{background: "#A7BF93", border: '1px solid #74634F'}}> submit </button>
         </div>
         
+      
+    
+      
       {notes.map((note) => {
         return (
-        <div className='bg-slate-500 mb-1 p-2 rounded-lg'> 
-
+        <div className=' mb-1 p-2 rounded-lg'style={{background: "#E4D292"}}> 
           <h1 className='text-left font-bold border-solid '> {note.Title} </h1> 
           <p className='text-left font-light border-2 border-sky-500 rounded-lg p-1 mb-2'> {note.Body} </p> 
 
           <div className='flex  flex-row justify-center gap-2 rounded-lg'> 
-            <button className='bg-red-300 p-1 rounded-lg' title='delete' onClick={() => {deleteNote(note.id)}}> Delete </button>
-          </div>   
+          <button className='p-1 rounded-lg' title='edit' onClick={() => {updateNote(note.id, note.Title, note.Body)}}  style={{background: "#9EB8C7"}}> Edit </button>
+          <button className='p-1 rounded-lg' title='delete' onClick={() => {deleteNote(note.id)}} style={{background: "#C7807B"}}> Delete </button>
+          </div>
+          
         </div>
         )
       })}
