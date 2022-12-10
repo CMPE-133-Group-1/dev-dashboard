02995540bba305b37ef9dcb73e733fb3404dc775
Cassidy <75055778+cassidychu@users.noreply.github.com>
2022-12-09 18:33:31 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import WeatherApp from './components/WeatherApp.jsx';
 import SnippetsApp from './components/SnippetsApp';
+import Calendar from './components/Calendar.jsx';
 import ColorWheel from './components/ColorWheel';
 import Banner  from './components/Banner.jsx';
 import NotesApp from './components/NotesApp';
@@ -7,23 +8,34 @@ import Timer  from './components/Timer.jsx';
 import Links from './components/Links.jsx';
 import ToDoApp from './components/ToDoApp';
 
-
+// installed an icon package refer: https://icons.radix-ui.com
 import './App.css';
 
 function App() {
   return (
-    <div className="App flex justify-center  h-screen w-screen bg-gradient-to-br from-lime-600 to-green-400">
+    <div className="App flex justify-center  h-screen w-screen from-[#9DAB86]">
       <div className="w-4/5 h-full pt-16 ">
         <Banner/>
         <div className="grid grid-cols-3 gap-2 h-4/5">
-          <div className='grid grid-row gap-1'>
+          <div className='from-[#9DAB86] grid grid-row gap-1'>
             <Links/>
+            <ColorWheel/>
+            <NotesApp/>
+
           </div>
-          <div className=''> 
-           <WeatherApp/>
+          
+          <div className='flex flex-col justify-between'> 
+           <ToDoApp/>
+           <Calendar/>
+           <Timer/>
+            
+    
+           
           </div>
+
           <div className=''>
-            <Timer/>
+            <WeatherApp/>
+            <SnippetsApp/>
           </div>
         </div>
 
@@ -33,4 +45,5 @@ function App() {
 }
 
 export default App;
+
 
